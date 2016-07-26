d8d13c0c9eaeac6f6d64e26074f9db818667d670
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-07-26 10:57:26 +0200

    added some basic files for the project

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM pasmod/miniconder2
+
+RUN apt-get update && \
+	apt-get install -y build-essential python-dev && \
+	apt-get clean
+
+RUN conda install -y \
+  pip \
+  numpy \
+  scikit-learn \
+  nltk
+
+RUN python -c "import nltk; nltk.download('stopwords'); nltk.download('punkt'); nltk.download('wordnet')" 
+RUN pip install pytest
+RUN pip install pytest-pep8
+
+WORKDIR /var/www
+ADD . .
+RUN pip install -e .
+
+#RUN py.test --pep8
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='paradox',
+    version='0.0.1',
+    description='Paraphrase Detection for Hindi Languages',
+    url='http://dbs.cs.uni-duesseldorf.de',
+    author='HHU Duesseldorf',
+    author_email='modaresi@cs.uni-duesseldorf.de',
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=[]
+)
