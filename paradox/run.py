9e76259c41aa1327cc52c8744055c288651cf5d9
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-13 18:45:41 +0200

    Parse all four corpora

--- paradox/run.py
+++ paradox/run.py
@@ -1,4 +1,8 @@
 from paradox.parsers.corpus_parser import parse
 
 
-X, y = parse(path='../corpora/Malayalam/dpil-mal-train-Task1.xml')
+X_malayalam_task1, y_malayalam_task1 = parse(path='../corpora/Malayalam/dpil-mal-train-Task1.xml')
+X_malayalam_task2, y_malayalam_task2 = parse(path='../corpora/Malayalam/dpil-mal-train-Task2.xml')
+
+X_tamil_task1, y_tamil_task1 = parse(path='../corpora/Tamil/dpil-tam-train-Task1.xml')
+X_tamil_task2, y_tamil_task2 = parse(path='../corpora/Tamil/dpil-tam-train-Task2.xml')
