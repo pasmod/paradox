f85884be89f554780de30b8d21e71fbb180492d8
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-13 18:36:59 +0200

    Fix path to corpus

--- paradox/run.py
+++ paradox/run.py
@@ -1,4 +1,4 @@
 from paradox.parsers.corpus_parser import parse
 
 
-X, y = parse(path='corpora/Malayalam/dpil-mal-train-Task1.xml')
+X, y = parse(path='../corpora/Malayalam/dpil-mal-train-Task1.xml')
