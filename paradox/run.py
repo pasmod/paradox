3cdf7cc6211c31bd5bbd47b0974407e876064053
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-16 09:43:59 +0200

    Fix pep8 offenses

--- paradox/run.py
+++ paradox/run.py
@@ -1,8 +1,13 @@
 from paradox.parsers.corpus_parser import parse
 
-
 X_malayalam_task1, y_malayalam_task1 = parse(path='../corpora/Malayalam/dpil-mal-train-Task1.xml')
 X_malayalam_task2, y_malayalam_task2 = parse(path='../corpora/Malayalam/dpil-mal-train-Task2.xml')
 
 X_tamil_task1, y_tamil_task1 = parse(path='../corpora/Tamil/dpil-tam-train-Task1.xml')
 X_tamil_task2, y_tamil_task2 = parse(path='../corpora/Tamil/dpil-tam-train-Task2.xml')
+
+X_hindi_task1, y_hindi_task1 = parse(path='../corpora/Hindi/dpil-hindi-train-Task1.xml')
+X_hindi_task2, y_hindi_task2 = parse(path='../corpora/Hindi/dpil-hindi-train-Task2.xml')
+
+X_punjabi_task1, y_punjabi_task1 = parse(path='../corpora/Punjabi/dpil-punjabi-train-Task1.xml')
+X_punjabi_task2, y_punjabi_task2 = parse(path='../corpora/Punjabi/dpil-punjabi-train-Task2.xml')
