c9f4c08613ba18b1737e98028d7f33ec9d397d84
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-16 15:42:56 +0200

    Add GridSearch for baseline approach

--- paradox/evaluation/count_vectorizer_word_baseline.py
+++ paradox/evaluation/count_vectorizer_word_baseline.py
@@ -4,12 +4,21 @@ from sklearn.svm import SVC
 from sklearn.pipeline import FeatureUnion
 from sklearn.base import BaseEstimator, TransformerMixin
 from metrics import calculate_and_print_metrics
+from sklearn.grid_search import GridSearchCV
 
 
 def estimate_svm_baseline(test_train_split):
     pipeline = create_pipeline(test_train_split)
-    pipeline.fit(test_train_split['X_train'], test_train_split['y_train'])
-    predicted_classes = pipeline.predict(test_train_split['X_test'])
+
+    param_grid = {'svm__C': [1e3, 5e3, 1e4, 5e4, 1e5],
+                  'svm__gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]}
+    print("Starting grid search")
+    grid_search = GridSearchCV(pipeline, param_grid, scoring='accuracy', cv=3)
+    grid_search.fit(test_train_split['X_train'], test_train_split['y_train'])
+    predicted_classes = grid_search.predict(test_train_split['X_test'])
+    # classification
+    # pipeline.fit(test_train_split['X_train'], test_train_split['y_train'])
+    # predicted_classes = pipeline.predict(test_train_split['X_test'])
     calculate_and_print_metrics(test_train_split['y_test'], predicted_classes)
 
 
@@ -27,7 +36,7 @@ def create_pipeline(test_train_split):
                 ])),
             ],
         )),
-        ('clf', SVC()),
+        ('svm', SVC()),
     ])
 
 
