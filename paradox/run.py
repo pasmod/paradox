aac706be0b652cf8bfdca40add3688da9b05f7d3
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-16 14:47:23 +0200

    Change encoding of console

--- paradox/run.py
+++ paradox/run.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
 from parsers.corpus_parser import parse
 from evaluation.training_test_split import split_training_data
+from evaluation.count_vectorizer_word_baseline import estimate_svm_baseline
+from tokenizers.hindi_tokenizer import Tokenizer
+
+import codecs
+import sys
+
+UTF8Writer = codecs.getwriter('utf8')
+sys.stdout = UTF8Writer(sys.stdout)
 
 X_malayalam_task1, y_malayalam_task1 = parse(path='../corpora/Malayalam/dpil-mal-train-Task1.xml')
 X_malayalam_task2, y_malayalam_task2 = parse(path='../corpora/Malayalam/dpil-mal-train-Task2.xml')
@@ -13,9 +22,5 @@ X_hindi_task2, y_hindi_task2 = parse(path='../corpora/Hindi/dpil-hindi-train-Tas
 X_punjabi_task1, y_punjabi_task1 = parse(path='../corpora/Punjabi/dpil-punjabi-train-Task1.xml')
 X_punjabi_task2, y_punjabi_task2 = parse(path='../corpora/Punjabi/dpil-punjabi-train-Task2.xml')
 
-
 test_train_split_malayalam_task1 = split_training_data(X_malayalam_task1, y_malayalam_task1)
-print(len(test_train_split_malayalam_task1['X_train']))
-print(len(test_train_split_malayalam_task1['X_test']))
-print(len(test_train_split_malayalam_task1['y_train']))
-print(len(test_train_split_malayalam_task1['y_test']))
+estimate_svm_baseline(test_train_split_malayalam_task1)
