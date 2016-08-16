0a0d791f1ffc49430d1a747ca4fb3f22840ef414
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-16 11:21:57 +0200

    Fix typo

--- paradox/evaluation/training_test_split.py
+++ paradox/evaluation/training_test_split.py
@@ -1,7 +1,7 @@
 from sklearn.cross_validation import train_test_split
 
 
-def split_training_data(X, ygi):
+def split_training_data(X, y):
     random_state = 123456
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=random_state)
     return {'X_train': X_train,
