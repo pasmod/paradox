ecc264f7e924350a139c3fa17971d2e79b7a7544
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-16 11:23:48 +0200

    Test training + test split

--- paradox/run.py
+++ paradox/run.py
@@ -1,4 +1,5 @@
-from paradox.parsers.corpus_parser import parse
+from parsers.corpus_parser import parse
+from evaluation.training_test_split import split_training_data
 
 X_malayalam_task1, y_malayalam_task1 = parse(path='../corpora/Malayalam/dpil-mal-train-Task1.xml')
 X_malayalam_task2, y_malayalam_task2 = parse(path='../corpora/Malayalam/dpil-mal-train-Task2.xml')
@@ -11,3 +12,10 @@ X_hindi_task2, y_hindi_task2 = parse(path='../corpora/Hindi/dpil-hindi-train-Tas
 
 X_punjabi_task1, y_punjabi_task1 = parse(path='../corpora/Punjabi/dpil-punjabi-train-Task1.xml')
 X_punjabi_task2, y_punjabi_task2 = parse(path='../corpora/Punjabi/dpil-punjabi-train-Task2.xml')
+
+
+test_train_split_malayalam_task1 = split_training_data(X_malayalam_task1, y_malayalam_task1)
+print(len(test_train_split_malayalam_task1['X_train']))
+print(len(test_train_split_malayalam_task1['X_test']))
+print(len(test_train_split_malayalam_task1['y_train']))
+print(len(test_train_split_malayalam_task1['y_test']))
