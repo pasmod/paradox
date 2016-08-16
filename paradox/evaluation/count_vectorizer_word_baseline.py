a55fec3321d3f8556b72f561334f904ae5f6b757
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-16 15:16:56 +0200

    Add the prediction and printing of results

--- paradox/evaluation/count_vectorizer_word_baseline.py
+++ paradox/evaluation/count_vectorizer_word_baseline.py
@@ -3,10 +3,18 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import SVC
 from sklearn.pipeline import FeatureUnion
 from sklearn.base import BaseEstimator, TransformerMixin
+from metrics import calculate_and_print_metrics
 
 
 def estimate_svm_baseline(test_train_split):
-    pipeline = Pipeline([
+    pipeline = create_pipeline(test_train_split)
+    pipeline.fit(test_train_split['X_train'], test_train_split['y_train'])
+    predicted_classes = pipeline.predict(test_train_split['X_test'])
+    calculate_and_print_metrics(test_train_split['y_test'], predicted_classes)
+
+
+def create_pipeline(test_train_split):
+    return Pipeline([
         ('union', FeatureUnion(
             transformer_list=[
                 ('sentence_a', Pipeline([
@@ -21,7 +29,6 @@ def estimate_svm_baseline(test_train_split):
         )),
         ('clf', SVC()),
     ])
-    pipeline.fit(test_train_split['X_train'], test_train_split['y_train'])
 
 
 class ItemSelector(BaseEstimator, TransformerMixin):
