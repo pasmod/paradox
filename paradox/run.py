23540cab716d755e0bac261b3c1a3088d781f731
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-18 13:09:54 +0200

    Refactor the choice of the evaluation method, fixes #6

--- paradox/run.py
+++ paradox/run.py
@@ -8,13 +8,21 @@ import sys
 UTF8Writer = codecs.getwriter('utf8')
 sys.stdout = UTF8Writer(sys.stdout)
 
-data_sets = load_all_languages()
 
+def evaluate(test_train_split, method_name):
+    if method_name == 'svm_baseline_default_tokenizer':
+        estimate_svm_baseline(test_train_split)
+    elif method_name == 'svm_baseline_hindi_tokenizer':
+        estimate_svm_baseline(test_train_split, True)
+
+
+data_sets = load_all_languages()
+evaluation_method_name = 'svm_baseline_hindi_tokenizer'
 for language, language_data_set in data_sets.iteritems():
     print 'Evaluating {}'.format(language)
     for x in range(1, 3):
         key = 'Task{}'.format(x)
         print('>> ' + key)
         test_train_split = split_training_data(language_data_set[key][0], language_data_set[key][1])
-        estimate_svm_baseline(test_train_split)
+        evaluate(test_train_split, evaluation_method_name)
     print ''
