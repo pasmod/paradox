fc62c93d05cbffba93c6cd1e750f226cdcae496b
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-19 16:29:16 +0200

    Add evaluation metrics to the final output

--- paradox/keras_test.py
+++ paradox/keras_test.py
@@ -8,36 +8,46 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
+from evaluation.metrics import calculate_and_print_metrics
 from keras.optimizers import SGD
 import codecs
 import sys
 import numpy as np
+from keras.utils.np_utils import to_categorical
 
 UTF8Writer = codecs.getwriter('utf8')
 sys.stdout = UTF8Writer(sys.stdout)
 
 data_sets = load_all_languages()
-language = 'Hindi'
-test_train_split = split_training_data(data_sets[language]['Task1'][0], data_sets[language]['Task1'][1])
+language = 'Punjabi'
+task = 'Task2'
+test_train_split = split_training_data(data_sets[language][task][0], data_sets[language][task][1])
 
 vocabulary = get_vocabulary(test_train_split['X_train'], analyzer='char_wb')
 
 # transform sentences to character one-hot-encodings
 
+number_of_classes = 3
+
 X_train = get_one_hot_encoding(test_train_split['X_train'], vocabulary=vocabulary, analyzer='char_wb')
 y_train = test_train_split['y_train']
+#print len(X_train)
 X_test = get_one_hot_encoding(test_train_split['X_test'], vocabulary=vocabulary, analyzer='char_wb')
 y_test = test_train_split['y_test']
+# print len(y_test)
+
 
 y_train = np.array(y_train)
-y_test = np.array(y_test)
+y_train = to_categorical(y_train, number_of_classes)
+y_test_categorical = to_categorical(np.array(y_test), number_of_classes)
 
 # print y_train.shape
+#np.random.seed(123456)
 
-
-batch_size = 32
+batch_size = 128
 nb_epoch = 20
 length_input_layer = len(vocabulary) * 2
+
 
 # ****************************
 model = Sequential()
@@ -51,14 +61,26 @@ model.add(Dense(64, init='uniform'))
 model.add(Activation('tanh'))
 model.add(Dropout(0.5))
 model.add(Dense(10, init='uniform'))
-model.add(Activation('softmax'))
+#model.add(Dropout(0.5))
+#model.add(Dense(2, activation='sigmoid'))
+model.add(Dense(number_of_classes, activation='softmax'))
+# model.add(Activation('tanh'))
+#model.add(Activation('softmax'))
 
 sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='sparse_categorical_crossentropy',
+model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
 
 model.fit(X_train, y_train,
           nb_epoch=nb_epoch,
           batch_size=batch_size)
-score = model.evaluate(X_test, y_test, batch_size=batch_size)
+
+predicted_values = model.predict(X_test)
+predicted_classes = np.argmax(predicted_values, axis=1)
+
+
+calculate_and_print_metrics(y_test, predicted_classes)
+score = model.evaluate(X_test, y_test_categorical, batch_size=batch_size)
+
+print score
