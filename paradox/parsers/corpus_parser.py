5848e375bcc866351122e984005402526c6e7239
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-19 13:02:24 +0200

    Change class name to numeric, needed for Keras

--- paradox/parsers/corpus_parser.py
+++ paradox/parsers/corpus_parser.py
@@ -3,6 +3,7 @@ import logging
 
 
 def parse(path=None):
+    map_class_numeric = {'P': 0, 'NP': 1, 'SP': 2} # Needed for keras
     e = xml.etree.ElementTree.parse(path).getroot()
     logging.info('parsing data set: name={}, version={}'.format(e.get('name'), e.get('version')))
     corpus = e.findall('Corpus')[0]
@@ -16,7 +17,8 @@ def parse(path=None):
         sentence2 = p.find("Sentence2").text
         clazz = p.find("Class").text
         X.append((sentence1, sentence2))
-        y.append(clazz)
+        # y.append(clazz)
+        y.append(map_class_numeric[clazz])
         logging.debug('parsed pair with id: {}'.format(p_id))
     logging.info('parsed {} pairs of sentences'.format(len(paraphrases)))
     return X, y
