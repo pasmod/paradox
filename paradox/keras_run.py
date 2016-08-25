52c03578997ce7ef66e6f37471c5f860a6a2a587
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-25 15:39:03 +0200

    Add model checkpoint, fixes #16

--- paradox/keras_run.py
+++ paradox/keras_run.py
@@ -7,6 +7,7 @@ from encoders import models
 from evaluation.metrics import evaluate_keras_predictions
 from keras.optimizers import SGD, Adagrad, Adam
 import time
+from keras.callbacks import ModelCheckpoint
 
 number_of_classes = 2
 language = 'Tamil'
@@ -21,9 +22,13 @@ def run_simple_model(number_of_classes=number_of_classes, language=language, dat
 
     model.compile(loss='categorical_crossentropy', metrics=['accuracy'],
                   optimizer=Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08))
-    model.fit(data_set['X_train'], data_set['y_train'],
-              nb_epoch=nb_epoch,
-              batch_size=batch_size)
+    checkpoint = ModelCheckpoint("/var/www/trained_models/temp/simple_encoder.model."
+                                 "{epoch:03d}-{val_acc:.4f}.hdf5",
+                                 monitor='val_acc', verbose=1,
+                                 save_best_only=True, mode='auto')
+    model.fit(data_set['X_train'], data_set['y_train'], callbacks=[checkpoint], verbose=1,
+              validation_data=(data_set['X_test'], data_set['y_test_categorical']),
+              nb_epoch=nb_epoch, batch_size=batch_size)
     evaluate_keras_predictions(data_set['y_test'], model.predict(data_set['X_test']))
     print model.evaluate(data_set['X_test'], data_set['y_test_categorical'], batch_size=batch_size)
     return model
