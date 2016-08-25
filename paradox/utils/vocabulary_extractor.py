22251aebc949095a5567bc983fc3ccb15834a513
Matthias Liebeck <Liebeck@users.noreply.github.com>
2016-08-25 12:45:07 +0200

    Add ngram range to vocabulary_extractor

--- paradox/utils/vocabulary_extractor.py
+++ paradox/utils/vocabulary_extractor.py
@@ -1,8 +1,8 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-def get_vocabulary(X_tuples, analyzer='word', tokenizer=None):
+def get_vocabulary(X_tuples, analyzer='word', tokenizer=None, ngram_range=(1, 1)):
     X = [(' '.join(x)) for x in X_tuples]
-    vectorizer = CountVectorizer(analyzer=analyzer, tokenizer=tokenizer)
+    vectorizer = CountVectorizer(analyzer=analyzer, tokenizer=tokenizer, ngram_range=ngram_range)
     vectorizer.fit(X)
     return vectorizer.get_feature_names()
