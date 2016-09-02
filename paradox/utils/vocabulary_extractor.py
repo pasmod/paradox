32ab59a9d5601ae6ee8499796714820e39e862d1
Pashutan Modaresi <p.modaresi@gmx.de>
2016-09-02 11:34:00 +0200

    fixed a bug in get_vocabulary

--- paradox/utils/vocabulary_extractor.py
+++ paradox/utils/vocabulary_extractor.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 def get_vocabulary(X_tuples, analyzer='word', tokenizer=None, ngram_range=(1, 1)):
     a = [(x[0]) for x in X_tuples]
-    b = [(x[0]) for x in X_tuples]
+    b = [(x[1]) for x in X_tuples]
     vectorizer = CountVectorizer(analyzer=analyzer, tokenizer=tokenizer, ngram_range=ngram_range)
     vectorizer.fit(a)
     a_names = set(vectorizer.get_feature_names())
