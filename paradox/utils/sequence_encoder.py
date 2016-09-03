c285ca209d3baf1cd4cad6b468182e2fdd430492
Pashutan Modaresi <p.modaresi@gmx.de>
2016-09-03 13:45:35 +0200

    added custom tokenizer callable

--- paradox/utils/sequence_encoder.py
+++ paradox/utils/sequence_encoder.py
@@ -1,33 +1,11 @@
 import numpy as np
 np.set_printoptions(threshold=np.inf)
 from sklearn.feature_extraction.text import CountVectorizer
-from paradox.tokenizers.hindi_tokenizer_wrapper import hindi_tokenize
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
 
-def _encode_sequence(X, max_length, vocabulary, tokenizer=hindi_tokenize, concat_vectors=True):
-    tokenized_sentences_A = [(tokenizer(x[0])) for x in X]
-    tokenized_sentences_B = [(tokenizer(x[1])) for x in X]
-    encodings_sentences_A = []
-    encodings_sentences_B = []
-    padded_A = pad(tokenized_sentences_A, max_length)
-    padded_B = pad(tokenized_sentences_B, max_length)
-    vocabulary = {x: i for i, x in enumerate(vocabulary)}
-    for i in range(len(padded_A)):
-        s1 = [vocabulary.get(word, vocabulary.get('<UNK/>')) for word in padded_A[i]]
-        s2 = [vocabulary.get(word, vocabulary.get('<UNK/>')) for word in padded_B[i]]
-        encodings_sentences_A.append(s1)
-        encodings_sentences_B.append(s2)
-    encodings_sentences_A = np.array(encodings_sentences_A)
-    encodings_sentences_B = np.array(encodings_sentences_B)
-    if concat_vectors:
-        return np.concatenate((encodings_sentences_A, encodings_sentences_B), axis=1)
-    else:
-        return [encodings_sentences_A, encodings_sentences_B]
-
-
-def encode_sequence(X, max_length, vocabulary, tokenizer=hindi_tokenize, concat_vectors=True):
+def encode_sequence(X, max_length, vocabulary, concat_vectors=True):
     t = Tokenizer()
     vectorizer = CountVectorizer(analyzer='word', vocabulary=vocabulary, binary=True)
     sentences_A = [x[0] for x in X]
--- paradox/utils/vocabulary_extractor.py
+++ paradox/utils/vocabulary_extractor.py
@@ -1,13 +1,16 @@
 from sklearn.feature_extraction.text import CountVectorizer
 
+def tokenize(text):
+    return text.split(" ")
+
 
 def get_vocabulary(X_tuples, analyzer='word', tokenizer=None, ngram_range=(1, 1)):
     a = [(x[0]) for x in X_tuples]
     b = [(x[1]) for x in X_tuples]
-    vectorizer = CountVectorizer(analyzer=analyzer, tokenizer=tokenizer, ngram_range=ngram_range)
+    vectorizer = CountVectorizer(analyzer=analyzer, tokenizer=tokenize, ngram_range=ngram_range)
     vectorizer.fit(a)
     a_names = set(vectorizer.get_feature_names())
-    vectorizer = CountVectorizer(analyzer=analyzer, tokenizer=tokenizer, ngram_range=ngram_range)
+    vectorizer = CountVectorizer(analyzer=analyzer, tokenizer=tokenize, ngram_range=ngram_range)
     vectorizer.fit(b)
     b_names = set(vectorizer.get_feature_names())
     return list(a_names | b_names)
