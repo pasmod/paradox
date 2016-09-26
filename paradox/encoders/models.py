29add56c026d813f046337cdc0f6aea7aac04443
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-09-26 11:12:24 +0200

    refactored the models files

--- paradox/encoders/models.py
+++ paradox/encoders/models.py
@@ -1,23 +1,38 @@
+from keras.layers import Dense, Embedding
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.layers import Convolution1D, LSTM, Embedding, Merge
+from keras.layers import LSTM
+from keras.layers import Merge
 
 
-def lstm_branch_approach(vocabulary_size, sequence_length, number_of_classes):
+def lstm_encoder(encoding_dim=32, depth=3, activation='relu',
+                 max_len=None, max_features=None, embeddings=None,
+                 vocab=None, lang='en', weights=None):
     left_branch = Sequential()
-    left_branch.add(LSTM(128, input_shape=(sequence_length, vocabulary_size),
-                         return_sequences=True))
-    left_branch.add(LSTM(64, return_sequences=True))
+    if lang == 'en':
+        left_branch.add(Embedding(len(vocab) + 1, 200, input_length=max_len,
+                                  weights=[embeddings],
+                                  trainable=True))
+    if lang == 'de':
+        left_branch.add(Embedding(weights.shape[0], weights.shape[1],
+                                  input_length=max_len,
+                                  weights=[weights],
+                                  trainable=True))
     right_branch = Sequential()
-    right_branch.add(LSTM(128, input_shape=(sequence_length, vocabulary_size),
-                          return_sequences=True))
-    right_branch.add(LSTM(64, return_sequences=True))
+    if lang == 'en':
+        right_branch.add(Embedding(len(vocab) + 1, 200, input_length=max_len,
+                                   weights=[embeddings],
+                                   trainable=True))
+    if lang == 'de':
+        right_branch.add(Embedding(weights.shape[0], weights.shape[1],
+                                   input_length=max_len,
+                                   weights=[weights],
+                                   trainable=True))
     merged = Merge([left_branch, right_branch], mode='concat')
     model = Sequential()
     model.add(merged)
-    model.add(LSTM(64, dropout_W=0.8, return_sequences=True))
-    model.add(LSTM(32, dropout_W=0.8))
-    model.add(Dense(32))
-    model.add(Dense(number_of_classes))
-    model.add(Activation('softmax'))
+    model.add(LSTM(128, dropout_W=0.5, return_sequences=True))
+    model.add(LSTM(64, dropout_W=0.5))
+    model.add(Dense(2, activation='softmax'))
+    model.compile(optimizer='adadelta', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
     return model
