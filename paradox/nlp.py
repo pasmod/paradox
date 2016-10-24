b274a3dd50c80aef4287fe994d49b3cb1c9451bb
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-10-24 15:43:53 +0200

    write wrapper for rouge in nlp

--- paradox/nlp.py
+++ paradox/nlp.py
@@ -1,6 +1,9 @@
-def ngrams(tokens, n):
-    return zip(*[tokens[i:] for i in range(n)])
+from pythonrouge import pythonrouge
 
 
-def tokenize(text):
-    return text.split(" ")
+def rouge(text1, text2, method='ROUGE-2'):
+    methods = ['ROUGE-1', 'ROUGE-2', 'ROUGE-3', 'ROUGE-SU4', 'ROUGE-L']
+    if method not in methods:
+        raise ValueError("Method {} is not supported."
+                         "Available methods: {}".format(method, methods))
+    return pythonrouge.pythonrouge(text1, text2)[method]
