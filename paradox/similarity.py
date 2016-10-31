37f9fa50f4c4dabab28d3f8f7e3e20d86995ce49
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-10-31 09:24:23 +0100

    remove print statements

--- paradox/similarity.py
+++ paradox/similarity.py
@@ -17,16 +17,9 @@ def surface(text1, text2, method='ROUGE-2'):
 
 def context(text1, text2):
     tokens1 = preprocess(text1)
-    print tokens1
     tokens2 = preprocess(text2)
-    print tokens2
     vectors1 = filter(lambda v: v, [glove.vector(token) for token in tokens1])
-    print len(vectors1)
     vectors2 = filter(lambda v: v, [glove.vector(token) for token in tokens2])
-    print vectors2
     center1 = np.average(np.array(vectors1), axis=0)
-    print len(vectors2)
-    print center1
     center2 = np.average(np.array(vectors2), axis=0)
-    print center2
     return 1 - cosine(center1, center2)
