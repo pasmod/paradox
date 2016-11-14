a9c45806e7a1635ad7f182e8d4ba063b9638e90d
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-11-14 09:56:32 +0100

    improve logging in parser

--- paradox/parser.py
+++ paradox/parser.py
@@ -1,9 +1,10 @@
 import logging
 import json
+import os
 
 
-def parse(mapping="corpus/mapping.json"):
-    mapping = json.loads(open(mapping).read())
+def parse(mapping="corpus/mapping.json", mode='train'):
+    mapping = json.loads(open(mapping).read())[mode]
     pairs = []
     for k, v in mapping.iteritems():
         pairs.extend(_parse(rawfile_path=k, gsfile_path=v))
@@ -11,11 +12,12 @@ def parse(mapping="corpus/mapping.json"):
 
 
 def _parse(rawfile_path=None, gsfile_path=None):
-    logging.info("Parsing file: {}".format(rawfile_path))
     pairs = []
-    with open(rawfile_path) as raw_file:
+    with open(os.path.join("corpus", rawfile_path)) as raw_file:
+        logging.info("Parsing raw file: {}".format(rawfile_path))
         raw_lines = raw_file.readlines()
-        with open(gsfile_path) as gs_file:
+        with open(os.path.join("corpus", gsfile_path)) as gs_file:
+            logging.info("Parsing gs file: {}".format(gsfile_path))
             gs_lines = gs_file.readlines()
             for raw_line, gs_line in zip(raw_lines, gs_lines):
                 if gs_line.replace("\n", "").isdigit():
