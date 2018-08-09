ad0bc75d0e4786b170c54986902136830259e4fd
Al Johri <al.johri@gmail.com>
2018-08-08 23:02:00 -0400

    more python3 compact

--- paradox/parser.py
+++ paradox/parser.py
@@ -12,8 +12,8 @@ def parse(mapping="corpus/mapping.json", mode='train',
             pairs.extend(_parse(rawfile_path=k, gsfile_path=v))
     if mode == 'test':
         for c in categories:
-            pairs.extend(_parse(rawfile_path=mapping[c].keys()[0],
-                                gsfile_path=mapping[c].values()[0]))
+            pairs.extend(_parse(rawfile_path=list(mapping[c].keys())[0],
+                                gsfile_path=list(mapping[c].values())[0]))
     return pairs
 
 
